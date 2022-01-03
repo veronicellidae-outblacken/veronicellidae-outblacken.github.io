74e279d6f7cdda7b95309a2544269cc9320c2f27
acurtis <alan.curtis@raytheon.com>
2022-01-03 18:22:07 -0500

    geneweb

---
+++ cgi-bin/test-cgi.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+echo 'Content-type: text/html'
+echo  
+echo '<!DOCTYPE html>'
+echo '<html xmlns="http://www.w3.org/1999/xhtml">'
+echo '<body>'
+echo 'This is a test for cgi commands'
+echo '</body>'
+echo '</html>'
